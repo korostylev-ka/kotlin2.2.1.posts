47132ef92968acb10df077e9dfade214ff50f0c6
korostylev-ka <k.korostylev@gmail.com>
2022-07-02 23:22:32 +0500

    Added Task1, Task2, Task3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 data class Post(
    val id: Int, //Идентификатор записи.
    val ownerId: Int, //Идентификатор владельца стены, на которой размещена запись
@@ -10,21 +12,26 @@ data class Post(
    val friendsOnly: Boolean, // если запись была создана с опцией «Только для друзей».
    val comments: Comments, //Информация о комментариях к записи, объект с полями:
    val copyright: Copyright, //Источник материала, объект с полями:
-   val likes: Likes,    //Информация о лайках к записи, объект с полями:
-   val reposts: Reposts,    //Информация о репостах записи («Рассказать друзьям»), объект с полями:
-   val views: Views,    //Информация о просмотрах записи. Объект с единственным полем:
+   var likes: Likes,    //Информация о лайках к записи, объект с полями:
+   var reposts: Reposts,    //Информация о репостах записи («Рассказать друзьям»), объект с полями:
+   var views: Views,    //Информация о просмотрах записи. Объект с единственным полем:
    val postType: String, //Тип записи, может принимать следующие значения: post, copy, reply, postpone, suggest.
    val signerId: Int,   //Идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем
-   //val copyHistory: Array<T>, //Массив, содержащий историю репостов для записи.
    val canPin: Boolean, //Информация о том, может ли текущий пользователь закрепить запись
-   val canDelete: Boolean,
+   val canDelete: Boolean, //Информация о том, может ли текущий пользователь удалить запись
+   val canEdit: Boolean, //Информация о том, может ли текущий пользователь редактировать запись
+   val isPinned: Boolean,   //Информация о том, что запись закреплена
+   val markedAsAds: Boolean, //Информация о том, содержит ли запись отметку "реклама"
+   val isFavorite: Boolean, //если объект добавлен в закладки у текущего пользователя
+   val donut: Donut //Информация о записи VK Donut
 
+) {
+    /*override fun toString(): String {
+        val postPrint = ("id поста: $id , текст поста: $text")
+        return postPrint
+    }*/
+}
 
-
-
-
-
-)
 //Информация о комментариях к записи, объект с полями:
 data class Comments(
     val count: Int, //количество комментариев
@@ -60,5 +67,171 @@ data class Reposts(
 data class Views(
     val count: Int //число просмотров записи
 )
+
+//Информация о записи VK Donut
+data class Donut(
+    val isDonut: Boolean,   //запись доступна только платным подписчикам VK Donut
+    val paidDuration: Int,  // время, в течение которого запись будет доступна только платным подписчикам VK Donut
+    val canPublish_free_copy: Boolean,  //можно ли открыть запись для всех пользователей, а не только подписчиков VK Donut;
+    val editMode: String    //информация о том, какие значения VK Donut можно изменить в записи. Возможные значения:
+
+)
+
+object WallService {
+
+    private var posts = emptyArray<Post>()  //массив с постами
+    private var isOriginId = true //поле для проверки поста на повтор
+    private var origId = 0 //уникальный id
+    private var isUpdate = false
+
+    //метод очистки массива
+    fun erasePosts(): Unit {
+        posts = emptyArray()
+    }
+
+    fun getOriginId(): Int {
+        return origId
+    }
+    fun add(post: Post): Post {
+        //добавляем первый пост
+        if (posts.size == 0) {
+            posts += post
+            origId += 1
+        } else {
+            for (post2 in posts) {
+                //проверка на оригинальность по id поста
+                if (post.id == post2.id) isOriginId = false
+
+            }
+            //если пост оригинальный, добавляем в массив и изменяем уникальный id
+            if (isOriginId == true) {
+                posts += post
+                origId += 1
+            }
+        }
+        return post
+    }
+
+    //Обновление записи
+    fun update(post: Post): Boolean{
+        for ((index, postCheck) in posts.withIndex()) {
+            //проверка на оригинальность по id поста
+            if (postCheck.id == post.id) {
+                posts[index] = post.copy(fromId = postCheck.fromId, date = postCheck.date)
+                isUpdate = true
+            }
+
+        }
+        return isUpdate
+    }
+}
+
+fun main() {
+    var postOne = Post(
+        1,
+        1111,
+        1111,
+        1111,
+        1656697687,
+        "message1",
+        1110,
+        2,
+        false,
+        Comments(25,true,true,true,true),
+        Copyright(11,"www.netology.ru","data class","Post"),
+        Likes(15,true,true,true),
+        Reposts(15,true),
+        Views(1000),
+        "post",
+        1111,
+        true,
+        true,
+        true,
+        false,
+        false,
+        true,
+        Donut(false,0,false,""))
+
+    var postTwo = Post(
+        2,
+        1112,
+        1112,
+        1112,
+        1656697687,
+        "message2",
+        1111,
+        1,
+        false,
+        Comments(29,true,true,true,true),
+        Copyright(12,"www.netology.ru","Services","Post"),
+        Likes(35,true,true,true),
+        Reposts(14,true),
+        Views(1200),
+        "post",
+        1112,
+        true,
+        true,
+        true,
+        false,
+        false,
+        true,
+        Donut(false,0,false,""))
+
+    var postThree = Post(
+        3,
+        1113,
+        1113,
+        1113,
+        1656771927,
+        "message3",
+        1112,
+        2,
+        true,
+        Comments(29,true,true,true,true),
+        Copyright(12,"www.netology.ru","Services","Post"),
+        Likes(37,true,true,true),
+        Reposts(104,true),
+        Views(2300),
+        "post",
+        1112,
+        true,
+        true,
+        true,
+        false,
+        false,
+        true,
+        Donut(false,0,false,""))
+
+    var postOneUpdate = Post(
+        1,
+        2222,
+        2221,
+        2222,
+        1656771927,
+        "message1",
+        1110,
+        2,
+        false,
+        Comments(45,true,true,true,true),
+        Copyright(11,"www.netology.ru","data class","Post"),
+        Likes(145,true,true,true),
+        Reposts(25,true),
+        Views(700),
+        "post",
+        1111,
+        true,
+        true,
+        true,
+        false,
+        false,
+        true,
+        Donut(false,0,false,""))
+
+    WallService.add(postOne)
+    WallService.add(postTwo)
+    //проверяем, что повторно пост с имеющимся id не добавится
+    WallService.add(postTwo)
+
+}
 
 
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,130 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @Test
+    fun add() {
+        val startId = WallService.getOriginId()
+        //проверяем начальное значение id
+        assertEquals(0,startId)
+        val  postAdd = WallService.add(Post(
+            1,
+            1111,
+            1111,
+            1111,
+            1656697687,
+            "message1",
+            1110,
+            2,
+            false,
+            Comments(25,true,true,true,true),
+            Copyright(11,"www.netology.ru","data class","Post"),
+            Likes(15,true,true,true),
+            Reposts(15,true),
+            Views(1000),
+            "post",
+            1111,
+            true,
+            true,
+            true,
+            false,
+            false,
+            true,
+            Donut(false,0,false,"")))
+        val nextId = WallService.getOriginId()
+        //проверяем id после добавления поста
+        assertEquals(1,nextId)
+    }
+
+    @Test
+    fun update() {
+        val  postOne = WallService.add(Post(
+            1,
+            1111,
+            1111,
+            1111,
+            1656697687,
+            "message1",
+            1110,
+            2,
+            false,
+            Comments(25,true,true,true,true),
+            Copyright(11,"www.netology.ru","data class","Post"),
+            Likes(15,true,true,true),
+            Reposts(15,true),
+            Views(1000),
+            "post",
+            1111,
+            true,
+            true,
+            true,
+            false,
+            false,
+            true,
+            Donut(false,0,false,"")))
+
+        val  postTwo = WallService.add(Post(
+            2,
+            1112,
+            1112,
+            1112,
+            1656697687,
+            "message2",
+            1111,
+            1,
+            false,
+            Comments(29,true,true,true,true),
+            Copyright(12,"www.netology.ru","Services","Post"),
+            Likes(35,true,true,true),
+            Reposts(14,true),
+            Views(1200),
+            "post",
+            1112,
+            true,
+            true,
+            true,
+            false,
+            false,
+            true,
+            Donut(false,0,false,"")))
+
+        val postOneUpdate = Post(
+            1,
+            2222,
+            2221,
+            2222,
+            1656771927,
+            "message1",
+            1110,
+            2,
+            false,
+            Comments(45,true,true,true,true),
+            Copyright(11,"www.netology.ru","data class","Post"),
+            Likes(145,true,true,true),
+            Reposts(25,true),
+            Views(700),
+            "post",
+            1111,
+            true,
+            true,
+            true,
+            false,
+            false,
+            true,
+            Donut(false,0,false,""))
+        //очищаем массив
+        WallService.erasePosts()
+        //добавляем пост
+        WallService.add(postOne)
+        //пытаемся изменить пост с новым id
+        val falseUpdate = WallService.update(postTwo)
+        assertFalse(falseUpdate)
+        //пытаемся изменить пост с существующим id
+        val trueUpdate = WallService.update(postOneUpdate)
+        assertTrue(trueUpdate)
+    }
+
+
+}
